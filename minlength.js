3621e5759921acfe9833437194703ef02c28df8a
Fawk-Lee <54290507+Fawk-Lee@users.noreply.github.com>
2019-09-05 09:06:14 +0100

    Contact form update

---
+++ minlength.js
@@ -0,0 +1,8 @@
+$(document) .ready(function(){
+	$('form teaxtrea[minlength]').on('keyup', function(){
+		e_len = $(this).val().trim().length
+		e_min_len = Number ($(this).attr('minlength'))
+		message = e_min_len <=e_len ? '' : e_min_len + 'characters minimum'
+		this.setCustomValidity(message)
+	})
+})
